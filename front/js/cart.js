e96a3419680e4085d98c6b1ddcc24586fea8c31b
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2022-11-06 23:40:33 +0100

    Suppression du localStorage

--- front/js/cart.js
+++ front/js/cart.js
@@ -21,7 +21,36 @@
 // </article>
 
 let cartItems = document.getElementById("cart__items");
-console.log(cartItems);
 
-let itemsInCart = localStorage.getItem("cart");
-console.log(itemsInCart)
+// Fonction qui permet de récupérer le panier
+
+let cart = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : [];
+
+console.log(cart);
+for (let i in cart) {
+    console.log(cart[i])
+  cartItems.innerHTML += `<article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+    <div class="cart__item__img">
+      <img src="../images/product01.jpg" alt="Photographie d'un canapé">
+    </div>
+    <div class="cart__item__content">
+      <div class="cart__item__content__description">
+        <h2>${cart[i].name}</h2>
+        <p>Vert</p>
+        <p>${cart[i].price} €</p>
+      </div>
+      <div class="cart__item__content__settings">
+        <div class="cart__item__content__settings__quantity">
+          <p>Qté : </p>
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+        </div>
+        <div class="cart__item__content__settings__delete">
+          <p class="deleteItem">Supprimer</p>
+        </div>
+      </div>
+    </div>
+    </article>
+    `;
+}
--- front/js/script.js
+++ front/js/script.js
@@ -1,10 +1,12 @@
+
+
 ajaxGet("http://localhost:3000/api/products", function (reponse) {
   let products = JSON.parse(reponse);
   console.log(products);
   // Affichage des produits
   let productDisplay = document.getElementById("items");
   products.map((product) => {
-    productDisplay.innerHTML += `<a href="./front/html/product.html?id=${product._id}">
+    productDisplay.innerHTML += `<a href="./product.html?id=${product._id}">
     <article>
       <img src="${product.imageUrl}" alt=${product.name}>
       <h3 class="productName">${product.name}</h3>
